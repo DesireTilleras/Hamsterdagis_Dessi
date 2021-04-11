66d90a1eef62253fdc88d118616739683527b116
Desire Tillerås <desire.tilleras@gmail.com>
2021-04-11 14:03:07 +0200

    ..

--- Hamsterdagis_Dessi/Program.cs
+++ Hamsterdagis_Dessi/Program.cs
@@ -14,8 +14,8 @@ namespace Hamsterdagis_Dessi
             }
             Simulation simulation = new Simulation();
             PrintToConsole printToConsole = new PrintToConsole();
+            simulation.ClearLoggFile();
 
-  
             Console.WriteLine("Welcome to Hamster Day Care Simulator!\n" +
                 "\nHow many days do you want to simulate?");
             int days = int.Parse(Console.ReadLine());
@@ -23,7 +23,7 @@ namespace Hamsterdagis_Dessi
             Console.WriteLine("\n\nTo pause the simulation : Press enter");
             Console.WriteLine("How long do you want the simulation to take? Please answer in whole minutes ");
             int minutes = int.Parse(Console.ReadLine());
-            simulation.ClearLoggFile();
+            
             simulation.ReportEventHandler += printToConsole.PrintReport;
             simulation.EndOfDayReport += printToConsole.PrintEndOfDay;
             simulation.TimeReport += printToConsole.PrintTime;
